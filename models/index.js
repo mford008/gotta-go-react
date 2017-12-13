9fbc34c4390db2352bcc84e7f47629c83fbc90c3
mrdannyb <mr.dannyb@gmail.com>
2017-12-12 19:54:45 -0800

    mongoose models

---
+++ models/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Restroom: require('./Restroom.js'),
+  Comment: require('./Comment.js')
+}
---
+++ models/restroom.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const restroomSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  category: {
+    type: String
+  },
+  comments: [{
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.Now
+  }
+});
+
+const Restroom = mongoose.model('Restroom', RestroomSchema);
+
+module.exports = Restroom;
