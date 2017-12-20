e1c310ec84ba6f60b2dfd280ba51d51904139a02
mrdannyb <mr.dannyb@gmail.com>
2017-12-19 18:27:53 -0800

    added some index.js files

---
+++ client/src/components/Map/Map.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Map = () => {
+  function initMap() {
+    var myLatLng = {lat: -25.363, lng: 131.044};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: myLatLng
+    });
+
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      title: 'Hello World!'
+    });
+  }
+
+  return (
+    <iframe
+src="https://www.google.com/maps/embed/v1/place?q=Uluru&key=AIzaSyAO6DPj2O5ahIC8Pr36AL6nQDUH0APPmyM" allowfullscreen></iframe>
+  )
+}
+
+
+export default Map;
