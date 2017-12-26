083ef677211bea4f43f4fe5aa4edea25a4f871f8
Maddy Ford <mrhf008@gmail.com>
2017-12-26 11:51:14 -0800

    attempting to resize navbar and keep active appropriately

--- client/src/components/Header/NavMenu.js
+++ client/src/components/Header/NavMenu.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { NavbarItem, NavbarLink, NavbarDropdown } from 'bloomer';
+import './Header.css';
 
 export const NavMenu = props =>
-  <NavbarItem hasDropdown isHoverable isHidden='mobile' style={{ background: '#39405f' }}>
+  <NavbarItem hasDropdown isHoverable isHidden='mobile'>
     <NavbarLink style={{ background: '#39405f', color: '#ffffff', fontWeight: '700' }} href=''>Menu</NavbarLink>
     <NavbarDropdown style={{ background: '#39405f' }}>
       {props.children}
