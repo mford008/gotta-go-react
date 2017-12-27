960cd5456e198aa5b54b89f90477a14ae3b589e8
beldrevo <beldrevo.by@gmail.com>
2017-12-27 01:23:00 -0800

    minor fixes

--- client/src/pages/AddLoo/AddLoo.js
+++ client/src/pages/AddLoo/AddLoo.js
@@ -2,16 +2,46 @@ import React, { Component } from 'react';
 import { Btn, InputField, Category, Comment, Form } from '../../components/Form';
 import { Title } from '../../components/Title/Title.js';
 import { Header } from '../../components/Header';
+import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'react-google-maps';
 
 class AddLoo extends Component {
+  state = {
+    name: '',
+    address: '',
+    category: '',
+    comment: ''
+  };
+
+  handleInputChange = event => {
+    // let value = event.target.value;
+    // const name = event.target.name;
+    //
+    // if (name === 'Address') {
+    //   value = new google.maps.places.Autocomplete();
+    // }
+  };
+
   render () {
     return (
       <div style={{ backgroundColor: '#f9fafb', width: '100%', height: '100%' }} className='content'>
         <Header />
         <Title>ADD A RESTROOM</Title>
         <Form>
-          <InputField type='text' placeholder='e.g. park, cafe' label='Location name' />
-          <InputField type='text' placeholder='Enter address' label='Address' />
+          <InputField
+            value={this.state.name}
+            name='name'
+            type='text'
+            placeholder='e.g. park, cafe'
+            label='Location name'
+          />
+          <InputField
+            value={this.state.address}
+            name='address'
+            type='text'
+            placeholder='Enter address'
+            label='Address'
+            onChange={this.handleInputChange}
+          />
           <Category />
           <Comment />
           <Btn onClick={this.handleFormSubmit}> Add Restroom </Btn>
