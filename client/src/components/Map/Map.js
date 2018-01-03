5de3dbb831cd3106ac61f763cb7d74513c9c7af1
beldrevo <beldrevo.by@gmail.com>
2018-01-03 15:31:43 -0800

    map component is rendered, added new UserGeo component

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -1,18 +1,21 @@
-import React, { Component } from 'react';
-import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import React from "react"
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
 
-// export const Map = withGoogleMap(props =>
-//   <GoogleMap
-//     defaultZoom={12}
-//     defaultCenter={{ lat: 37.8720727, lng: -122.2712664 }}
-//   >
-//     <Marker
-//       position={{ lat: 37.8720727, lng: -122.2712664 }}
-//     />
-//   </GoogleMap>
-// );
-//
-// <Map
-//   containerElement={<div style={{ height: `400px` }} />}
-//   mapElement={<div style={{ height: `100%` }} />}
-// />
+export const Map = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDCtDV0aY7IdfyS0HPw6M0AmJaQe9DPTTY&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `580px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat: 37.8720727, lng:  -122.2712664 }}
+  >
+    <Marker position={{ lat: 37.8720727, lng:  -122.2712664 }} />
+  </GoogleMap>
+)
