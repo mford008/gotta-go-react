1d290941a067c9d4ff4ea73d536052f9feada2bf
Alison Lawyer <alison@airbears2-10-142-112-224.airbears2.1918.berkeley.edu>
2018-01-04 21:05:43 -0800

    ignore these

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Navbar } from 'bloomer';
 import { Plus, NavbarLogo, NavbarMenu, MenuItem } from '../../components/Header';
 
-export const Header = props =>
-  <Navbar style={{backgroundColor: '#393f60', justifyContent: 'space-between'}}>
+class Header extends Component {
+  state = {
+    isHidden: ''
+  }
 
-    <Plus />
-    <NavbarLogo />
-    <NavbarMenu>
-      <MenuItem href='/landing'>Nearby</MenuItem>
-      <MenuItem href='/account'>Account</MenuItem>
-      <MenuItem href='/login'>Log in</MenuItem>
-    </NavbarMenu>
-  </Navbar>;
+  // onClickBurger = () => {
+  //   if (this.state.isHidden) {
+  //     this.setState.isHidden =
+  //   }
+  // }
+
+  render () {
+    return (
+      <Navbar style={{backgroundColor: '#393f60', justifyContent: 'space-between'}}>
+
+        <Plus />
+        <NavbarLogo />
+        <NavbarMenu>
+          <MenuItem href='/landing'>Nearby</MenuItem>
+          <MenuItem href='/account'>Account</MenuItem>
+          <MenuItem href='/login'>Log in</MenuItem>
+        </NavbarMenu>
+      </Navbar>
+    );
+  }
+}
+
+export default Header;
