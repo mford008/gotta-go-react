df95ebaf0e0eb37cbcee85223f1728ab7e1701d6
Maddy Ford <mrhf008@gmail.com>
2018-01-06 13:20:19 -0800

    database testing with seeds

--- client/src/pages/AddLoo/AddLoo.js
+++ client/src/pages/AddLoo/AddLoo.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Btn, InputField, Category, Comment, Form, AutocompleteInput } from '../../components/Form';
 import { Title } from '../../components/Title/Title.js';
-import { Header } from '../../components/Header';
+import Header from '../../components/Header/Header.js';
 import { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 import API from '../../utils/API';
 
