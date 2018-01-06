d6f55b3c6b0acfdad461879243e01048c2df6a79
mrdannyb <mr.dannyb@gmail.com>
2018-01-06 15:23:23 -0800

    landing page now with list and map components

---
+++ client/src/pages/Landing/LandingList.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import '../Login/Login.css';
+import API from '../../utils/API';
+import { ListContainer, ListItem, CommentContainer, CommentItem } from "../../components/List";
+import Header from '../../components/Header/Header.js';
+import { Link } from 'react-router-dom';
+import { TabGroup, SingleTab } from '../../components/TabGroup';
+
+class LandingList extends Component {
+  state = {
+    restroomList: []
+  }
+
+  componentDidMount () {
+    API.loadRestrooms()
+    // .then(res => console.log(res.data))
+    .then(res => this.setState({ restroomList: res.data }))
+  }
+
+  restroomLink = string => {
+    let link = "https://www.google.com/maps/place/";
+    link += string;
+    return link;
+  }
+
+  render () {
+    console.log(this.state.currentLocation);
+    return (
+      <div>
+        <ListContainer>
+          {this.state.restroomList.map(restroom => (
+            <ListItem key={restroom._id}>
+              <a href={'/restroom/' + restroom._id}>
+              <h1><strong>{restroom.name}</strong></h1></a>
+              <h3>
+                <a href={this.restroomLink(restroom.location)}>
+                  {restroom.location}
+                </a>
+              </h3>
+              <h2>{restroom.category}</h2>
+              <h3>Hours: {restroom.hours}</h3>
+              <h3>Rating: {restroom.rating}</h3>
+              <h4><strong>Comments:</strong> {restroom.comments.map(comments =>
+                (comments.ref)
+              )}</h4>
+            </ListItem>
+          ))}
+        </ListContainer>
+
+      </div>
+    );
+  }
+}
+
+export default LandingList;
