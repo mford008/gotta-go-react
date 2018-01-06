e46049a55b27b0f366b9133036be781912733c9f
beldrevo <beldrevo.by@gmail.com>
2018-01-06 10:25:03 -0800

    added Redux store, fixed autocomplete dropdown, added spinner to Start page (doesn't work yet)

---
+++ client/src/components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export class Spinner extends Component {
+
+  render () {
+    return (
+      <div className='has-text-centered spinnerDiv'>
+        <img className="spinner" src="./Rolling.svg"></img>
+      </div>
+    )
+  }
+}
