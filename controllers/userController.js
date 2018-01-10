140ac33d94b5cbae4cbc3d97bef0cf5a236e774c
mrdannyb <mr.dannyb@gmail.com>
2018-01-09 21:27:45 -0800

    working with usercontroller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,24 @@
-const db = require('../models');
+const db = require('../models/');
+const passport = require('../passport');
 
 module.exports = {
   create: (req, res) => {
-    console.log('req', req.body);
+    console.log('usercontroller req', req.body);
+    const { username, password } = req.body;
     db.User
-      .create(req.body)
+      .findOne({ 'local.username': username}, (err,userMatch) => {
+        if (userMatch) {
+          return res.json({ error: "username taken"})
+        }
+        const newUser = new User({
+          'local.username':username,
+          'local.password': password
+        })
+        newUser.save((err, savedUser) => {
+          if (err) return res.json(err)
+          return res.json(savedUser)
+        })
+      })
       .then(dbModel => res.json(dbModel))
       .catch(err => res.json(err));
   }
