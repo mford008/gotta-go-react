f7715179a1924590ce07e92238ca033624e59e36
Maddy Ford <mrhf008@gmail.com>
2018-01-11 19:12:02 -0800

    clearing fields after submit for adding restroom, login, and signup

--- client/src/pages/AddLoo/AddLoo.js
+++ client/src/pages/AddLoo/AddLoo.js
@@ -28,7 +28,7 @@ class AddLoo extends Component {
     geocodeByAddress(this.state.address)
       .then(results => getLatLng(results[0]))
       .then(latLng => console.log('Success', latLng))
-      .then(this.setState({name: '', location: '', comment: ''}))
+      .then(this.setState({name: '', location: '', category: '', comment: ''}))
       .catch(error => console.error('Error', error))
 
     geocodeByAddress(this.state.location)
