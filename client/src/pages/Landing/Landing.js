30cd70a383fc90e049f7eac2fdd3a47f3d52b80b
beldrevo <beldrevo.by@gmail.com>
2018-01-12 20:47:17 -0800

    some map changes

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -5,8 +5,27 @@ import Header from '../../components/Header/Header.js';
 import LandingMap from './LandingMap.js';
 import LandingList from './LandingList.js';
 
+
 class Landing extends Component {
+  state = {
+
+  }
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const coords = pos.coords;
+      if (this.state !== coords) {
+        this.setState({coords})
+        localStorage.setItem('lat', this.state.coords.latitude)
+        localStorage.setItem('lng', this.state.coords.longitude)
+        console.log(this.state);
+        console.log(localStorage);
+      }
+   })
+  }
+
   render () {
+
     return (
       <div>
         <Header />
