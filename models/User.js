395db2c9f4f960946b6013ee1985e173119b8ad8
Maddy Ford <mrhf008@gmail.com>
2018-12-11 13:22:28 -0800

    using bcrypt-nodejs instead of bcryptjs

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt-nodejs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
